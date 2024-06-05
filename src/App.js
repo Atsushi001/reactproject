14f273e585378adaa0ddb46406a050ad37e97488
Atsushi001 <moskvi4effm@yandex.ru>
2024-06-05 13:25:32 +0700

    2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import SiteHead from './AppHead'
 
 export default function App() {
   return (
+    <div className='App'>
     <SiteHead/>
+    </div>
   );
 }
 
---
+++ src/AppHead.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function SiteHead() {
+    return (
+        <header className="AppHeader">
+          <div className="container">
+            <div className="header_row"></div>
+            <div className="header_logo"></div>
+            <div className="header_nav"></div>
+          </div>
+        </header>
+    );
+  }
+
+export default SiteHead;
