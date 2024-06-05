1c05d96888a9f84ef19b96279848c71d8c7a1bd2
Atsushi001 <moskvi4effm@yandex.ru>
2024-06-05 22:14:20 +0700

    3

--- src/AppHead.jsx
+++ src/AppHead.jsx
@@ -1,12 +1,21 @@
 import React from "react"
+import logoImg from "./kartinki/Alica.jpg"
 
 function SiteHead() {
     return (
         <header className="AppHeader">
           <div className="container">
-            <div className="header_row"></div>
-            <div className="header_logo"></div>
-            <div className="header_nav"></div>
+            <div className="header_row">
+              <div className="header_logo"><img src={logoImg} alt="Logo" className="Alicka"></img></div>
+              <nav className="header_nav">
+                <ul> 
+                  <li><a href="#!" className="header_nav_btn">КНОПКА</a></li>
+                  <li><a href="#!" className="header_nav_btn">КНОПКА</a></li>
+                  <li><a href="#!" className="header_nav_btn">КНОПКА</a></li>
+                  <li><button type="button" class="btn btn-dark header_nav_btn">Dark</button></li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </header>
     );
