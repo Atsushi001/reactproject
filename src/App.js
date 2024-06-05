419eb6b84c472c0799dbb5bbf625c8a0a87f1bca
Atsushi001 <moskvi4effm@yandex.ru>
2024-06-05 12:44:44 +0700

    1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import SiteHead from './AppHead'
+
+export default function App() {
+  return (
+    <SiteHead/>
+  );
+}
+
+
---
+++ src/AppHead.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function SiteHead() {
+    return (
+      <div>
+        <header className="AppHeader">
+        </header>
+      </div>
+    );
+  }
